4a08c9d93c3d69ee32916d1af3aea1bfabfc25e7
ousmane <sangaryousmane@gmail.com>
2023-12-28 14:55:57 +0800

    Add authentication service

---
+++ authentication-service/src/main/java/com/ousmane/authenticationservice/repository/UserRepository.java
@@ -0,0 +1,27 @@
+package com.ousmane.authenticationservice.repository;
+
+import com.ousmane.authenticationservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @Query("SELECT u FROM User u WHERE u.username=:username")
+    Optional<User> findByUsername(@Param("username") String username);
+
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN TRUE ELSE FALSE END" +
+            " FROM User u WHERE u.email=:email")
+    Boolean existsByEmail(@Param("email") String email);
+
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN TRUE ELSE FALSE END " +
+            "FROM User u WHERE u.username=:username")
+    Boolean existsByUsername(@Param("username") String username);
+
+    @Query("SELECT u FROM User u WHERE u.id=:id")
+    User findById(int userId);
+}
